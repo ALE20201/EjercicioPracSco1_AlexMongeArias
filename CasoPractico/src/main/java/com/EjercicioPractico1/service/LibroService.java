21610523acf2025db019e1f1d59886919fbd2931
Alex M <145505088+ALE20201@users.noreply.github.com>
2024-02-28 20:50:21 -0600

    comit1.0

---
+++ CasoPractico/src/main/java/com/EjercicioPractico1/service/LibroService.java
@@ -0,0 +1,11 @@
+package com.EjercicioPractico1.service;
+
+import com.EjercicioPractico1.domain.Libro;
+import java.util.List;
+
+public interface LibroService {
+    
+    // Se obtiene un listado de libros en un List
+    public List<Libro> getLibros();
+ 
+}
